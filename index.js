5b2ed88c88c1ec94be1eb61380527abb1ab5947e
kimcocru <kimcocru@gmail.com>
2018-01-22 11:42:45 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from "react-dom"
+
+
+//montar el todo básico
+const App = () => (
+
+       <h1>Yass Queen</h1>
+   )
+
+//el metodo Render imprime la aplicacion
+ReactDOM.render(<App/>, document.getElementById('root'))
